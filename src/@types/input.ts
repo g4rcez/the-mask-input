04445e59ca6522ee08f04a2a95ec3c4ad124cf84
g4rcez <allan.f.garcez@gmail.com>
2020-04-23 23:15:19 -0300

    refactor types and improvement on keyboard objects

--- src/@types/input.ts
+++ src/@types/input.ts
@@ -1,65 +1,55 @@
 export type InputTypes =
-  | "button"
-  | "checkbox"
-  | "color"
-  | "date"
-  | "datetime-local"
-  | "email"
-  | "file"
-  | "hidden"
-  | "image"
-  | "month"
-  | "number"
-  | "password"
-  | "radio"
-  | "range"
-  | "reset"
-  | "search"
-  | "submit"
-  | "tel"
-  | "text"
-  | "time"
-  | "url"
-  | "week";
+	| "button"
+	| "checkbox"
+	| "color"
+	| "date"
+	| "datetime-local"
+	| "email"
+	| "file"
+	| "hidden"
+	| "image"
+	| "month"
+	| "number"
+	| "password"
+	| "radio"
+	| "range"
+	| "reset"
+	| "search"
+	| "submit"
+	| "tel"
+	| "text"
+	| "time"
+	| "url"
+	| "week";
 
-export type InputMode =
-  | "decimal"
-  | "none"
-  | "text"
-  | "numeric"
-  | "tel"
-  | "search"
-  | "email"
-  | "url";
+export type InputMode = "decimal" | "none" | "text" | "numeric" | "tel" | "search" | "email" | "url";
 
-export type InputProps = Omit<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  "type" | "autoCapitalize"
-> & {
-  type?: InputTypes;
-  inputMode?: InputMode;
-  autoCapitalize?: "off" | "none" | "on" | "sentences" | "words" | "characters";
+export type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, "type" | "autoCapitalize"> & {
+	type?: InputTypes;
+	inputMode?: InputMode;
+	autoCapitalize?: "off" | "none" | "on" | "sentences" | "words" | "characters";
 };
 
 export type MasksTypes =
-  | "cellphone"
-  | "cellTelephone"
-  | "cep"
-  | "cnpj"
-  | "color"
-  | "cpf"
-  | "cpfCnpj"
-  | "creditCard"
-  | "currency"
-  | "date"
-  | "isoDate"
-  | "telephone";
+	| "cellphone"
+	| "cellTelephone"
+	| "cep"
+	| "cnpj"
+	| "color"
+	| "cpf"
+	| "cpfCnpj"
+	| "creditCard"
+	| "currency"
+	| "date"
+	| "isoDate"
+	| "pis"
+	| "telephone";
 
 export type DecimalKeyboard = Exclude<MasksTypes, "currency">;
 
 export type KeyboardProp = {
-  pattern: string;
-  title: string;
-  inputMode: InputMode;
+	pattern: string;
+	title: string;
+	inputMode: InputMode;
 };
 export type DecimalKeyboardProps = { [key in DecimalKeyboard]: KeyboardProp };
