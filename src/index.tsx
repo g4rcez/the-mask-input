9eb9d0ae1605d85f7692c7096016e90a9dfe096d
g4rcez <allan.f.garcez@gamil.com>
2020-05-22 12:15:15 -0300

    add exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export { Input as default } from "./components/input";
 export * from "./@types/currency-code";
 export * from "./@types/masks";
+export * from "./validate-input";
 export * from "./@types/input";
 export * from "./@types/locales";
 export * from "./components/currency-input";
