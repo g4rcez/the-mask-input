a02d65664f0a32c75f720e84238b8467103b6cb1
g4rcez <allan.f.garcez@gmail.com>
2020-08-21 23:56:45 -0300

    fix ref

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,12 @@
-import React, { useImperativeHandle, useMemo, useRef } from "react";
+import React, { useCallback, useImperativeHandle, useMemo, useRef } from "react";
 import MaskedInput from "react-text-mask";
 import { CustomInputProps, MaskConfig } from "../@types/input";
 import { convertMaskToString, maskConfig } from "../helpers/masks";
 import { CurrencyInput } from "./currency-input";
 
 export const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(({ mask, value = "", ...html }, externalRef) => {
-	const internalRef = useRef<HTMLInputElement>(null);
-	useImperativeHandle(externalRef, () => internalRef.current!);
+	const ref = useRef<HTMLInputElement>(null);
+	useImperativeHandle(externalRef, () => ref.current!);
 
 	const maskProps = useMemo(() => {
 		if (!(mask && typeof mask === "string" && mask in maskConfig)) {
@@ -31,13 +31,16 @@ export const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(({ mas
 	const masked = useMemo(() => (maskProps === null ? value : maskProps.convert(value)), [value, maskProps]);
 
 	if (mask === "currency") {
-		return <CurrencyInput {...html} value={value} ref={internalRef} />;
+		return <CurrencyInput {...html} value={value} ref={ref} />;
 	}
 
 	if (mask === undefined || maskProps === null) {
-		return <input {...html} value={value} ref={internalRef} />;
+		return <input {...html} value={value} ref={ref} />;
 	}
-
 	const { convert, mask: m, ...other } = maskProps;
-	return <MaskedInput {...html} {...other} guide={guide} mask={maskProps.mask} placeholder={placeholder} ref={internalRef as any} value={masked} />;
+	const forward = useCallback((e: MaskedInput | null) => {
+		(ref.current as any) = e?.inputElement;
+	}, []);
+	
+	return <MaskedInput {...html} {...other} guide={guide} mask={maskProps.mask} placeholder={placeholder} ref={forward} value={masked} />;
 });
