bb61c96ddcc4df66a57de90e9f6b0f103113b5b0
g4rcez <allan.f.garcez@gmail.com>
2020-09-25 18:34:48 -0300

    fix

--- src/components/currency-input.tsx
+++ src/components/currency-input.tsx
@@ -1,36 +1,42 @@
-import React, { useCallback, useImperativeHandle, useLayoutEffect, useRef, useState } from "react";
+import React, { useImperativeHandle, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { CurrencyInputProps } from "../@types/input";
-import { currencyToFloat } from "../helpers/fmt";
+import { currencyToFloat, namedFormatCurrency, toCurrency } from "../helpers/fmt";
 
 const PATTERN = "^[A-Z]{1,3}[0-9$,. ]+$";
 
 export const CurrencyInput = React.forwardRef<HTMLInputElement, CurrencyInputProps>(
 	({ locale = "pt-BR", currency = "BRL", adjustCaret, ...html }, externalRef) => {
 		const ref = useRef<HTMLInputElement>(null);
-		const [input, setInput] = useState<string>(() => `${html.value || ""}`);
-
-		const formatter = useCallback(
-			(n: string) => {
-				const realValue = currencyToFloat(n);
-				return new Intl.NumberFormat(locale, { style: "currency", currency }).format(realValue);
-			},
-			[locale, currency]
-		);
+		const [input, setInput] = useState<string>(() => `${html.value || "0"}`);
 
 		useLayoutEffect(() => {
-			const money = formatter(`${html.value || ""}`);
+			const realValue = currencyToFloat(`${html.value || "0"}`);
+			const money = Intl.NumberFormat(locale, { style: "currency", currency }).format(realValue);
 			setInput(money);
-		}, [html.value, formatter]);
+		}, []);
+
+		const info = useMemo(() => {
+			const infos = namedFormatCurrency(locale, currency);
+			infos.currency = `${infos.currency.trim()} `;
+			infos.literal = infos.literal.trim();
+			return infos;
+		}, [locale, currency]);
 
 		useImperativeHandle(externalRef, () => ref.current!);
 
 		const change = (e: React.ChangeEvent<HTMLInputElement>) => {
 			e.persist();
-			const money = formatter(e.target.value);
+			const money = toCurrency(e.target.value, {
+				separator: info.literal,
+				decimalSeparator: info.decimal,
+				prefix: info.currency,
+				decimalsLength: info.fraction.length
+			});
 			const realValue = currencyToFloat(money);
 			const cursor = e.target.selectionStart ?? 0;
-			ref.current!.value = money;
 			setInput(money);
+			ref.current!.value = money;
+			e.target.value = money;
 			html.onChange?.(e);
 			if (realValue !== 0) {
 				ref.current!.selectionEnd = cursor + 1;
